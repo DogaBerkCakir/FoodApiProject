a8c6bfc0c36fe859f33dc5ffdb3515072c0dfa3b
Doğa Berk Çakır <107003857+DogaBerkCakir@users.noreply.github.com>
2025-05-12 18:27:15 +0300

    UI tarafına basladikss :D

---
+++ FootApiProjectWebUI/ViewComponents/_HeadDefaultComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FootApiWebUI.ViewComponents
+{
+    public class _HeadDefaultComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
