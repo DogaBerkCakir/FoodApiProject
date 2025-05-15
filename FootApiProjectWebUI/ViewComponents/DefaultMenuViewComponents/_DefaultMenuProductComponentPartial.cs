b0c731880a17e888dd32ec0f0a61b5080b6bbfc6
Doğa Berk Çakır <107003857+DogaBerkCakir@users.noreply.github.com>
2025-05-15 18:01:30 +0300

    devam edecek....

---
+++ FootApiProjectWebUI/ViewComponents/DefaultMenuViewComponents/_DefaultMenuProductComponentPartial.cs
@@ -0,0 +1,31 @@
+﻿using FootApiWebUI.Dtos.CategoryDtos;
+using FootApiWebUI.Dtos.ProductDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace FootApiWebUI.ViewComponents.DefaultMenuViewComponents
+{
+    public class _DefaultMenuProductComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultMenuProductComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7102/api/Products");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync(); //API den geliyor
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData); // Dto ile yakala ve cshtml ye gonder seklinde calısırlar
+                return View(values);
+            }
+            return View();
+
+        }
+    }
+}
